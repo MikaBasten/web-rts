8092f1dfdeeda9d45f6c809bdcb6c0c70e89bc1a
Mika-basten <mci.basten@student.han.nl>
2024-11-22 12:04:13 +0100

    hope for the best

--- RTS-Server/RTS-Server/Dtos/LobbyDto.cs
+++ RTS-Server/RTS-Server/Dtos/LobbyDto.cs
@@ -3,8 +3,8 @@
     public class LobbyDto
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public int PlayerLimit { get; set; }
-        public List<PlayerDto> Players { get; set; }
+        public List<PlayerDto>? Players { get; set; }
     }
 }
